7eb9f9aec125712fb7198b61c9701e60f3eb66d7
SergSmilya <sergsmilichenko@gmail.com>
2023-01-20 17:39:18 +0200

    task 1, 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+  startEl: document.querySelector('[data-start]'),
+  stopEl: document.querySelector('[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function AddColorToBody() {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+refs.startEl.addEventListener('click', () => {
+  intervalId = setInterval(AddColorToBody, 1000);
+  refs.startEl.setAttribute('disabled', true);
+});
+
+refs.stopEl.addEventListener('click', () => {
+  clearInterval(intervalId);
+  refs.startEl.removeAttribute('disabled');
+});
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,18 @@
 import flatpickr from 'flatpickr';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const startBln = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+startBln.setAttribute('disabled', true);
+startBln.addEventListener('click', timerGo);
+
+let intervalId = null;
+let selectedDate = '';
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -12,6 +24,53 @@ const options = {
 };
 
 const flatpick = flatpickr('#datetime-picker', options);
+
 flatpick.config.onClose.push(function (selectedDates) {
-  console.log(selectedDates[0]);
+  selectedDate = selectedDates[0].getTime();
+  if (selectedDates[0] < new Date()) {
+    Notify.warning('Please choose a date in the future');
+  } else startBln.removeAttribute('disabled');
 });
+
+function timerGo() {
+  startBln.setAttribute('disabled', true);
+  intervalId = setInterval(() => {
+    const deltaTime = selectedDate - Date.now();
+    if (deltaTime > 0) {
+      const timerResult = convertMs(deltaTime);
+      timerNumbers(timerResult);
+    } else {
+      clearInterval(intervalId);
+      startBln.removeAttribute('disabled');
+    }
+  }, 1000);
+}
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+function timerNumbers({ days, hours, minutes, seconds }) {
+  daysEl.textContent = days;
+  hoursEl.textContent = hours;
+  minutesEl.textContent = minutes;
+  secondsEl.textContent = seconds;
+}
